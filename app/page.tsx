308c2d84939f7ec0372e2b88055d7bb639d808cb
Nick Wu <nickwu2005@icloud.com>
2024-10-08 00:24:33 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -24,6 +24,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[var(--font-koulen)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <TeamList teams={teams} /> {/* Use TeamList component to render teams */}
+        <button onClick={() => addTeam('New Team')}>Add Team</button>
       </main>
     </div>
   );
